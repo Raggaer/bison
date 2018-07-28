0ce7bf040c0597bae8fd884701efd9ee3d904350
Raggaer <nakotoffana@gmail.com>
2018-07-28 12:00:47 +0200

    Reload templates, files, routes while on dev mode one ach request

--- controller.go
+++ controller.go
@@ -21,6 +21,28 @@ type Handler struct {
 
 // MainRoute handles all http requests
 func (h *Handler) MainRoute(ctx *fasthttp.RequestCtx) {
+	// If we are running under development mode reload stuff
+	if h.Config.DevMode {
+		tpl, err := loadTemplates()
+		if err != nil {
+			ctx.Error("Unable to reload templates", 500)
+			return
+		}
+		h.Tpl = tpl
+		routes, err := loadRoutes()
+		if err != nil {
+			ctx.Error("Unable to reload routes", 500)
+			return
+		}
+		h.Routes = routes
+		luaFiles, err := lua.CompileFiles("controllers")
+		if err != nil {
+			ctx.Error("Unable to reload controllers", 500)
+			return
+		}
+		h.Files = luaFiles
+	}
+
 	// Retrieve current route
 	params := map[string]string{}
 	ctx.VisitUserValues(func(b []byte, i interface{}) {
